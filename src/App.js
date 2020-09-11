0bf2912c533293848d0e81411117c3a4d0fa5387
Suzanne Cabral <suzannecabral@live.com>
2020-09-11 14:14:03 -0700

    Added fake data and axios call. Need to acces axios results.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import DummyData from './components/dummyData'
+
+// console.log(DummyData)
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+// State: all characters
+  const [allChars, setAllChars] = useState([])
+  
+//State: active character
+  const[activeChar, setActiveChar] = useState(0)
+
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then(res => {
+        console.log('Here are the axios results:')
+        console.log(res)
+        setAllChars(res.data.results[0])
+
+        console.log('-------')
+        console.log('All Chars:')
+        console.log(allChars)
+        console.log(res.data.results)
+      })
+      .catch(err => {
+        console.log('-------There was a problem with the axios call:-------')
+        console.log(err)
+        debugger
+      })
+  }, [])
+
+
 
   return (
     <div className="App">
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    background-color:white;
+    border: 3px solid red;
+    border-radius:10px;
+`
+
+export default function Character({ info }) {
+    return (
+        <StyledCharacter>
+            <h2>{info.name}</h2>
+        </StyledCharacter>
+
+    )
+
+}
