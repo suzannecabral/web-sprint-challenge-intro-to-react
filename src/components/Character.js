e54b9cafa46ead65a8b45f284f82244e523dc8e6
Suzanne Cabral <suzannecabral@live.com>
2020-09-11 17:36:18 -0700

    Character cards render all info, MVP complete for resubmit

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 // Write your Character component here
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 
@@ -37,9 +37,11 @@ export default function Character({ info }) {
     // const homeworldUrl = info.homeworld;
     // const speciesUrl = info.species;
 
+    // let species = "human"
     // let homeworldName = "Earth"
-    let species = "human"
-    let homeworldName = "Earth"
+
+    const [homeworldName, setHomeworldName] = useState("Earth")
+    const [speciesName, setSpeciesName] = useState("human")
 
     // Axios.get(homeworldUrl)
     //     .then(res => {
@@ -65,16 +67,32 @@ export default function Character({ info }) {
             })
     }
 
+    function getSpecies(url){
+        return Axios.get(url)
+            .then(res => {
+                return res.data.name
+            })
+            .catch(err => {
+                console.log("CHARACTER: There was a problem with the Axios call------")
+                console.log(err)
+                debugger
+            })
+    }
+
     getHomeworld(info.homeworld)
         .then(data => {
-            homeworldName = data
-            console.log(homeworldName)
+            setHomeworldName(data)
         })
+
+    getSpecies(info.species)
+    .then(data => {
+        setSpeciesName(data)
+    })
 
     return (
         <StyledCharacter>
             <h2>{info.name}</h2>
-            <p>is a {info.species.length === 0 ? 'human' : species} from {homeworldName}</p>
+            <p>is a {info.species.length === 0 ? 'Human' : speciesName} from {homeworldName}</p>
         </StyledCharacter>
 
     )
